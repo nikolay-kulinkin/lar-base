3676ddecc262813b852dc7fa47206b100a8d2ea0
nikolay-kulinkin <kulinkin.nik@gmail.com>
2025-04-06 17:18:20 +0300

    Lesson 7

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyPlaceController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,4 @@ Route::get('/', function () {
 });
 Route::get('/my-page',[MyPlaceController::class,'index']);
 
+Route::get('/posts', [PostController::class,'index']);
