166e39222bf7c1df2524c76e3c541e7944173e14
nikolay-kulinkin <kulinkin.nik@gmail.com>
2025-04-06 21:11:28 +0300

    Lesson 15

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,50 +7,40 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-   public function index(){
-      // $post=Post::find(1);
-      // dd($post->title);
+   public function index()
+   {
       $posts = Post::all();
-      foreach($posts as $post){
-         dump($post->title);
-      }
-
-   //  $posts = Post::where('is_published', 1)->get();
-   //  foreach($posts as $post){
-   //    dump($post->title);
-   // //  }
-
-   // $post = Post::where('is_published', 1)->first();
-   // dd($post->title);
-   } 
+      return view('posts', compact('posts'));
+      
+   }
 
    public function create()
    {
       $postsArr = [
          [
-            'title'=>'title of post from phpstorm',
-            'content'=>'some interesting content',
-            'image'=>'imageblabla.jpg',
-            'likes'=>20,
-            'is_published'=>1,
+            'title' => 'title of post from phpstorm',
+            'content' => 'some interesting content',
+            'image' => 'imageblabla.jpg',
+            'likes' => 20,
+            'is_published' => 1,
          ],
          [
-            'title'=>'another title of post from phpstorm',
-            'content'=>'another some interesting content',
-            'image'=>'another imageblabla.jpg',
-            'likes'=>50,
-            'is_published'=>1,
+            'title' => 'another title of post from phpstorm',
+            'content' => 'another some interesting content',
+            'image' => 'another imageblabla.jpg',
+            'likes' => 50,
+            'is_published' => 1,
          ],
       ];
 
-      // Post::create([
-      //    'title'=>'another title of post from phpstorm',
-      //    'content'=>'another some interesting content',
-      //    'image'=>'another imageblabla.jpg',
-      //    'likes'=>50,
-      //    'is_published'=>1,
-      // ]);
-      
+      Post::create([
+         'title' => 'another title of post from phpstorm',
+         'content' => 'another some interesting content',
+         'image' => 'another imageblabla.jpg',
+         'likes' => 50,
+         'is_published' => 1,
+      ]);
+
       foreach ($postsArr as $item) {
          Post::create($item);
       }
@@ -58,14 +48,14 @@ class PostController extends Controller
 
    public function update()
    {
-      $post=Post::find(6);
+      $post = Post::find(6);
       // dd($post->title);
       $post->update([
-         'title'=>'updated',
-         'content'=>'updated',
-         'image'=>'updated',
-         'likes'=>1000,
-         'is_published'=>0
+         'title' => 'updated',
+         'content' => 'updated',
+         'image' => 'updated',
+         'likes' => 1000,
+         'is_published' => 0
       ]);
       dd('updated');
    }
@@ -80,29 +70,29 @@ class PostController extends Controller
       // $post->delete();
       // dd('softdelete');
 
-      $post=Post::withTrashed()->find(2);
+      $post = Post::withTrashed()->find(2);
       $post->restore();
    }
 
    public function firstOrCreate()
    {
-      $post=Post::firstOrCreate([
-      //    'title'=>'title of post from phpstorm'
-      // ],[
-      //    'title'=>'title of post from phpstorm',
-      //    'content'=>'some content',
-      //    'image'=>'some imageblabla.jpg',
-      //    'likes'=>50000,
-      //    'is_published'=>1
-      // ]);
+      $post = Post::firstOrCreate([
+         //    'title'=>'title of post from phpstorm'
+         // ],[
+         //    'title'=>'title of post from phpstorm',
+         //    'content'=>'some content',
+         //    'image'=>'some imageblabla.jpg',
+         //    'likes'=>50000,
+         //    'is_published'=>1
+         // ]);
 
-         'title'=>'some title phpstorm'
-      ],[
-         'title'=>'some title phpstorm',
-         'content'=>'some content',
-         'image'=>'some imageblabla.jpg',
-         'likes'=>50000,
-         'is_published'=>1
+         'title' => 'some title phpstorm'
+      ], [
+         'title' => 'some title phpstorm',
+         'content' => 'some content',
+         'image' => 'some imageblabla.jpg',
+         'likes' => 50000,
+         'is_published' => 1
       ]);
 
 
