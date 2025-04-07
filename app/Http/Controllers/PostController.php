3a0abd61a6ddb4723db95fb404806ed60aceaeb1
nikolay-kulinkin <kulinkin.nik@gmail.com>
2025-04-07 12:39:02 +0300

    Lesson 21

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,24 +11,14 @@ class PostController extends Controller
 {
    public function index()
    {
-      // $category = Category::find(1);
-      // dd($category->posts);
-
-      // $post=Post::find(1);
-      // dd($post->category);
-
-      // $post = Post::find(1);
-      // dd($post->tags);
-
-      $tag = Tag::find(1);
-      dd($tag->posts);
-      // return view('post.index', compact('posts'));
-      
+      $posts = Post::all();
+      return view('post.index', compact('posts'));
    }
 
    public function create()
    {
-      return view('post.create');
+      $categories = Category::all();
+      return view('post.create', compact('categories'));
    }
 
    public function store()
@@ -37,8 +27,10 @@ class PostController extends Controller
       
          'title'=>'string',
          'content'=>'string',
-         'image'=>'string'
+         'image'=>'string',
+         'category_id'=>''
       ]);
+      
       Post::create($data);
       return redirect()->route('post.index');
    }
@@ -50,8 +42,8 @@ class PostController extends Controller
 
    public function edit(Post $post)
    {
-      
-      return view('post.edit', compact('post'));
+      $categories = Category::all();
+      return view('post.edit', compact('post', 'categories'));
    }
 
    public function update(Post $post)
@@ -60,7 +52,8 @@ class PostController extends Controller
       $data=request()->validate([
          'title'=>'string',
          'content'=>'string',
-         'image'=>'string'
+         'image'=>'string',
+         'category_id'=>''
       ]);
       $post->update($data);
       return redirect()->route('post.show', $post);
--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -17,6 +17,17 @@
             <label for="image">Image</label>
             <input type="text" class="form-control" value="{{$post->image}}" name="image" id="image" placeholder="Image">
         </div>
+        <div class="form-group mb-3">
+            <label for="category">Category</label>
+            <select class="form-control" id="category" name="category_id">
+                @foreach($categories as $category)
+                <option
+                    {{$category->id === $post->category->id?'selected':''}}
+                    value="{{$category->id}}">{{$category->title}}
+                </option>
+                @endforeach
+            </select>
+        </div>
         <button type="submit" class="btn btn-primary mb-3">Update</button>
     </form>
 </div>
